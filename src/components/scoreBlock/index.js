8a89f8e7a067ed75e92e008ac2980f0d3d5b0286
lachlan mackenzie <lachlanrm@gmail.com>
2019-08-20 16:41:03 -0700

    [WIP]Add zone calculations

--- src/components/scoreBlock/index.js
+++ src/components/scoreBlock/index.js
@@ -27,35 +27,35 @@ class ScoreValue extends React.Component {
     console.log("time", time)
     switch (time) {
       case "zone7a":
-          return this.zoneCalcA(percentage)
+          return this.zoneCalc7A(percentage)
         break;
       case "zone7b":
-          return this.zoneCalcB(percentage)
+          return this.zoneCalc7B(percentage)
         break;
       case "zone6a":
-          return this.zoneCalcA(percentage)
+          return this.zoneCalc6A(percentage)
         break;
       case "zone6b":
-          return this.zoneCalcB(percentage)
+          return this.zoneCalc6B(percentage)
         break;
       case "zone5a":
-        return this.zoneCalcA(percentage)
+        return this.zoneCalc5A(percentage)
         break;
       case "zone5b":
-          return this.zoneCalcB(percentage)
+          return this.zoneCalc5B(percentage)
         break;
       case "zone4a":
-          return this.zoneCalcA(percentage)
+          return this.zoneCalc4A(percentage)
         break;
       case "zone4b":
-          return this.zoneCalcB(percentage)
+          return this.zoneCalc4B(percentage)
         break;
       default:
         break;
     }
   }
 
-  zoneCalcA = (percentage) => {
+  zoneCalc7A = (percentage) => {
     if ( ( percentage <= 5 ) && ( percentage <= 14) ){
       return "WAA"
     } else if ( ( percentage >= 15 ) && (percentage <= 21 ) ) {
@@ -71,7 +71,7 @@ class ScoreValue extends React.Component {
     } 
   }
 
-  zoneCalcB = (percentage) => {
+  zoneCalc7B = (percentage) => {
     if ( (percentage <= 8 ) && (percentage <= 19) ){
       return "WAA"
     } else if ( ( percentage >= 20 ) && ( percentage <= 34 ) ) {
@@ -82,6 +82,86 @@ class ScoreValue extends React.Component {
       return "BA"
     } else if ( ( percentage > 61 ) ) {
       return "WBA"
+    } else {
+      return ""
+    } 
+  }
+
+  zoneCalc6A = (percentage) => {
+    if ( (percentage <= 5 ) && (percentage <= 9) ){
+      return "WAA"
+    } else if ( ( percentage >= 10 ) && ( percentage <= 20 ) ) {
+      return "AA"
+    } else if ( ( percentage > 21 ) && ( percentage <= 24 ) ) {
+      return "A"
+    } else if ( ( percentage > 25 ) && ( percentage <= 30 ) ) {
+      return "BA"
+    } else if ( ( percentage > 31 ) ) {
+      return "WBA"
+    } else {
+      return ""
+    } 
+  }
+
+  zoneCalc6B = (percentage) => {
+    if ( (percentage <= 8 ) && (percentage <= 14) ){
+      return "WAA"
+    } else if ( ( percentage >= 15 ) && ( percentage <= 22 ) ) {
+      return "AA"
+    } else if ( ( percentage > 23 ) && ( percentage <= 35 ) ) {
+      return "A"
+    } else if ( ( percentage > 36 ) && ( percentage <= 50 ) ) {
+      return "BA"
+    } else if ( ( percentage > 51 ) ) {
+      return "WBA"
+    } else {
+      return ""
+    } 
+  }
+
+  zoneCalc5A = (percentage) => {
+    if ( (percentage <= 4 ) && (percentage <= 7) ){
+      return "AA"
+    } else if ( ( percentage >= 8 ) && ( percentage <= 14 ) ) {
+      return "A"
+    } else if ( ( percentage > 15 ) ) {
+      return "BA"
+    } else {
+      return ""
+    } 
+  }
+
+  zoneCalc5B = (percentage) => {
+    if ( (percentage <= 10 ) && (percentage <= 17) ){
+      return "AA"
+    } else if ( ( percentage >= 18 ) && ( percentage <= 23 ) ) {
+      return "A"
+    } else if ( ( percentage > 24 ) ) {
+      return "BA"
+    } else {
+      return ""
+    } 
+  }
+
+  zoneCalc4A = (percentage) => {
+    if ( (percentage <= 2 ) && (percentage <= 4) ){
+      return "AA"
+    } else if ( ( percentage >= 5 ) && ( percentage <= 6 ) ) {
+      return "A"
+    } else if ( ( percentage > 7 ) ) {
+      return "BA"
+    } else {
+      return ""
+    } 
+  }
+
+  zoneCalc4B = (percentage) => {
+    if ( (percentage <= 5 ) && (percentage <= 7) ){
+      return "AA"
+    } else if ( ( percentage >= 8 ) && ( percentage <= 14 ) ) {
+      return "A"
+    } else if ( ( percentage > 15 ) ) {
+      return "BA"
     } else {
       return ""
     } 
